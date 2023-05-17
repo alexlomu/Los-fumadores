4b76721a991bcc08897cfa3279ea0e4045cc5f64
alexlomu <91721507+alexlomu@users.noreply.github.com>
2023-05-17 22:08:23 +0200

    funciones fumador y agente ok

--- main.py
+++ main.py
@@ -3,13 +3,33 @@ import time
 import random
 
 # Creamos los semáforos
-ingredientes_sem = {
+ingredientes_semaforos = {
     'papel': threading.Semaphore(0),
     'tabaco': threading.Semaphore(0),
     'filtros': threading.Semaphore(0),
     'green': threading.Semaphore(0),
     'cerillas': threading.Semaphore(0)
 }
-
 puede_dejar_sem = threading.Semaphore(0)
+
+# Función de los fumadores
+def fumador(identificador, ingrediente_faltante):
+    while True:
+        ingredientes_semaforos[ingrediente_faltante].acquire()  
+        print(f'Fumador {identificador} toma los ingredientes y comienza a fumar')
+        time.sleep(2)  
+        print(f'Fumador {identificador} ha terminado de fumar')
+        puede_dejar_sem.release()  
+
+# Función del agente
+def agente():
+    while True:
+        ingredientes_dejados = random.sample(list(ingredientes_semaforos.keys()), 2)
+        print('El agente deja en la mesa:', ingredientes_dejados)
+        
+        for ingrediente in ingredientes_dejados:
+            ingredientes_semaforos[ingrediente].release()
+        
+        puede_dejar_sem.acquire()
+
 
